937c56a7ad6cb4a8f562142492493ed0cae429fd
ddigges <deborahgertrude_digges@intuit.com>
2017-03-07 20:09:40 +0530

    Add c++ gitignores

---
+++ P1-Extended-Kalman-Filter/class-notes/cpp-primer/src/hello.cpp
@@ -0,0 +1,14 @@
+//============================================================================
+// Name        : cpp-primer.cpp
+// Author      : Deborah Digges
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+
+int main() {
+	std::cout << "no more steering wheels" << std::endl;
+	return 0;
+}
