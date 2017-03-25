99e98834ee63285ca312de8c19ef394a84e6c5b3
ddigges <deborahgertrude_digges@intuit.com>
2017-03-25 16:31:27 +0530

    Add divide by zero check for h function

--- P1-Extended-Kalman-Filter/CarND-Extended-Kalman-Filter-Project/src/kalman_filter.cpp
+++ P1-Extended-Kalman-Filter/CarND-Extended-Kalman-Filter-Project/src/kalman_filter.cpp
@@ -23,7 +23,9 @@ void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
 }
 
 void KalmanFilter::Predict() {
+	std::cout << "X_:" <<  x_ << std::endl;
 	x_ = F_ * x_;
+	std::cout << "New X_:" <<  x_ << std::endl;
 	MatrixXd Ft = F_.transpose();
 	P_ = F_ * P_ * Ft + Q_;
 }
@@ -55,12 +57,17 @@ VectorXd h(VectorXd predicted_state) {
 	double vx = predicted_state[2];
 	double vy = predicted_state[3];
 
+	VectorXd projected_state = VectorXd(3);
+	projected_state << 0,0,0;
+
+	if(px == 0 && py == 0) {
+		return projected_state;
+	}
+
 	double rho = sqrt(pow(px, 2) + pow(py, 2));
 	double phi = atan(py/px);
-	std::cout << "PHI:"<< "::" << phi << std::endl;
 	double rho_dot = ((px * vx) + (py * vy))/rho;
 
-	VectorXd projected_state = VectorXd(3);
 	projected_state[0] = rho;
 	projected_state[1] = phi;
 	projected_state[2] = rho_dot;
@@ -86,19 +93,28 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
 	*/
 	// Use the non-linear function to project the predicted state
 	// into the measurement space
+	std::cout << "z:" << z << std::endl;
 	VectorXd z_pred = h(x_);
+	std::cout << "zpred:" << z_pred << std::endl;
 	VectorXd y = z - z_pred;
 
 	y[1] = SNormalizeAngle(y[1]);
+	std::cout << "y:" << y << std::endl;
 	MatrixXd Ht = H_.transpose();
+	std::cout << "Ht:" << Ht << std::endl;
 	MatrixXd S = H_ * P_ * Ht + R_;
+	std::cout << "S" << S << std::endl;
 	MatrixXd Si = S.inverse();
+	std::cout << "S_inv:" << Si << std::endl;
 	MatrixXd PHt = P_ * Ht;
+	std::cout << "PHt:" << PHt << std::endl;
 	MatrixXd K = PHt * Si;
-
+	std::cout << "K:" << K << std::endl;
 	//new estimate
 	x_ = x_ + (K * y);
+	std::cout << "x_:" << x_ << std::endl;
 	long x_size = x_.rows();
+	std::cout << "x_size" << x_size << std::endl;
 	MatrixXd I = MatrixXd::Identity(x_size, x_size);
 	P_ = (I - K * H_) * P_;
 }
