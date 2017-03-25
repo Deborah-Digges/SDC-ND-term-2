676bc4928f6be3f2c8625f1a253eb88486a8f3fd
ddigges <deborahgertrude_digges@intuit.com>
2017-03-25 12:38:55 +0530

    Add functions for non-linear transformation and normalization

--- P1-Extended-Kalman-Filter/CarND-Extended-Kalman-Filter-Project/src/kalman_filter.cpp
+++ P1-Extended-Kalman-Filter/CarND-Extended-Kalman-Filter-Project/src/kalman_filter.cpp
@@ -1,7 +1,12 @@
 #include "kalman_filter.h"
+#include <math.h>
+#include <iostream>
 
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
+
+VectorXd h(VectorXd);
+double SNormalizeAngle(double phi);
 
 KalmanFilter::KalmanFilter() {}
 
@@ -39,9 +44,62 @@ void KalmanFilter::Update(const VectorXd &z) {
 	P_ = (I - K * H_) * P_;
 }
 
+/*
+ * This takes a 4d vector [px, py, vx, vy] which represents
+ * the predicted state and returns the projection
+ * into the measurement space [rho, phi, rho dot]
+ */
+VectorXd h(VectorXd predicted_state) {
+	double px = predicted_state[0];
+	double py = predicted_state[1];
+	double vx = predicted_state[2];
+	double vy = predicted_state[3];
+
+	double rho = sqrt(pow(px, 2) + pow(py, 2));
+	double phi = atan(py/px);
+	std::cout << "PHI:"<< "::" << phi << std::endl;
+	double rho_dot = ((px * vx) + (py * vy))/rho;
+
+	VectorXd projected_state = VectorXd(3);
+	projected_state[0] = rho;
+	projected_state[1] = phi;
+	projected_state[2] = rho_dot;
+
+	return projected_state;
+
+}
+double SNormalizeAngle(double phi)
+{
+  const double Max = M_PI;
+  const double Min = -M_PI;
+
+  return phi < Min
+    ? Max + std::fmod(phi - Min, Max - Min)
+    : std::fmod(phi - Min, Max - Min) + Min;
+}
+
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
   /**
   TODO:
     * update the state by using Extended Kalman Filter equations
   */
+  // Use the non-linear function to project the predicted state
+  // into the measurement space
+//	VectorXd z_pred = h(x_);
+//	VectorXd y = z - z_pred;
+//
+//	y[1] = SNormalizeAngle(y[1]);
+//	MatrixXd Ht = H_.transpose();
+//	MatrixXd S = H_ * P_ * Ht + R_;
+//	MatrixXd Si = S.inverse();
+//	MatrixXd PHt = P_ * Ht;
+//	MatrixXd K = PHt * Si;
+//
+//	//new estimate
+//	x_ = x_ + (K * y);
+//	long x_size = x_.rows();
+//	MatrixXd I = MatrixXd::Identity(x_size, x_size);
+//	P_ = (I - K * H_) * P_;
 }
+
+
