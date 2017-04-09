6ca4aedb61e7f895ff6c05b129ab1cd030c41567
ddigges <deborahgertrude_digges@intuit.com>
2017-04-09 10:21:34 +0530

    Adjust yaw acceleration and longitudinal acceleration to pass rubric

--- P2-Unscented-Kalman-Filter/CarND-Unscented-Kalman-Filter-Project/src/ukf.cpp
+++ P2-Unscented-Kalman-Filter/CarND-Unscented-Kalman-Filter-Project/src/ukf.cpp
@@ -43,10 +43,10 @@ UKF::UKF() {
 	Xsig_pred_.fill(0.0);
 
 	// Process noise standard deviation longitudinal acceleration in m/s^2
-	std_a_ = 0.1;
+	std_a_ = 1.5;
 
 	// Process noise standard deviation yaw acceleration in rad/s^2
-	std_yawdd_ = 0.1;
+	std_yawdd_ = 0.55;
 
 	// Laser measurement noise standard deviation position1 in m
 	std_laspx_ = 0.15;
