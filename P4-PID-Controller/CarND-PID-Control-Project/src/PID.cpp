58a7b3e9e841c10c1b4b0ec3be921b56c274cb32
ddigges <deborahgertrude_digges@intuit.com>
2017-05-14 16:26:25 +0530

    Add 10^6 to PID

--- P4-PID-Controller/CarND-PID-Control-Project/src/PID.cpp
+++ P4-PID-Controller/CarND-PID-Control-Project/src/PID.cpp
@@ -20,7 +20,7 @@ void PID::Init(double Kp, double Ki, double Kd) {
 void PID::UpdateError(double cte) {
 	p_error = Kp * cte;
 	std::cout << "Kp, cte,p_error: "<< Kp << ", "<< cte << ", " << p_error << "\n";
-	d_error = Kd * (cte - this->prev_cte)/(CLOCKS_PER_SEC);
+	d_error = Kd * 1000000 *(cte - this->prev_cte)/(CLOCKS_PER_SEC);
 	std::cout << "cte, prev_cte, dt,d_errror: " << cte << "," << this->prev_cte << this->dt/CLOCKS_PER_SEC << ", " << d_error << "\n";
 	//i_error = Ki * cte;
 }
